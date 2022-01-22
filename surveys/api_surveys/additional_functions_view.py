23a97c5e96490e9bb35293b0c1b28f1cddffdf14
psg <korkhov.pi@gmail.com>
2022-01-22 22:44:17 +0300

    fix func checking_answers

--- surveys/api_surveys/additional_functions_view.py
+++ surveys/api_surveys/additional_functions_view.py
@@ -4,13 +4,9 @@ from rest_framework.response import Response
 def checking_answers(question_type, answers, choices_list, i):
     """ checking answers with options in the database """
     if question_type == 'Single choice answer':
-        if int(answers[0]['choices_answer_id']) in list(choices_list) and len(answers) == 1:
-            pass
-        else:
+        if int(answers[0]['choices_answer_id']) not in list(choices_list) and len(answers) != 1:
             return Response(data=f'Error, Answer to the question {i["question_id"]} incorrect')
     elif question_type == 'Multiple choice answer':
         for answer in answers:
-            if int(answer['choices_answer_id']) in list(choices_list):
-                continue
-            else:
+            if int(answer['choices_answer_id']) not in list(choices_list):
                 return Response(data=f'Error, Answer to the question {i["question_id"]} incorrect')
